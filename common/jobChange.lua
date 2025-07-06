c87a8444ddc577a44bd49d5380222c9b0987db27
avogadro-war <109310871+avogadro-war@users.noreply.github.com>
2025-07-05 20:08:42 -0500

    Add files via upload

---
+++ common/jobChange.lua
@@ -0,0 +1,36 @@
+local event = require('event');
+
+local onSubJobChange = event:new();
+local onMainJobChange = event:new();
+
+local lastMainJob, lastSubJob;
+
+lastMainJob = AshitaCore:GetMemoryManager():GetPlayer():GetMainJob();
+lastSubJob = AshitaCore:GetMemoryManager():GetPlayer():GetSubJob();
+ashita.events.register('packet_in', 'subjob_packet_in', function(e)
+    if (e.id == 0x061) then
+        local mainJob = struct.unpack('B', e.data, 0x0C + 0x01);
+        local subJob = struct.unpack('B', e.data, 0x0E + 0x01);
+
+        if (mainJob == 0) then return; end
+
+        if (mainJob and mainJob ~= lastMainJob) then
+            local job = AshitaCore:GetResourceManager():GetString("jobs.names_abbr", mainJob);
+
+            onMainJobChange:trigger(job);
+            lastMainJob = mainJob;
+        end
+
+        if (subJob and subJob ~= lastSubJob) then
+            -- print('job change from ' .. tostring(lastSubJob) .. ' to ' .. subJob)
+            local job = AshitaCore:GetResourceManager():GetString("jobs.names_abbr", subJob);
+            onSubJobChange:trigger(job);
+            lastSubJob = subJob;
+        end
+    end
+end);
+
+return {
+    onMainJobChange = onMainJobChange,
+    onSubJobChange = onSubJobChange
+};
