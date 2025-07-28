93181671c8fb21b24080ad2a9f974eabedb8b0ec
avogadro-war <109310871+avogadro-war@users.noreply.github.com>
2025-07-28 08:12:17 -0500

    Delete onevent2 directory

--- onevent2/triggers/bosses/thinker.lua
+++
@@ -1,8 +0,0 @@
-return T{
-    -- Pain Sync
-    { '[Glassy Thinker] Pain Sync', '/echo Turn!; sound:tf2.wav' },
-
-    -- Mighty Strikes
-    { '[Glassy Thinker] Mighty Strikes', '/echo Mighty Strikes!; sound:cyberpunk.wav' },
-
-}
