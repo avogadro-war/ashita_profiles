b5931b012f5fc7a607e5a2f416d72d50d37152d5
avogadro-war <109310871+avogadro-war@users.noreply.github.com>
2025-08-07 13:32:33 -0500

    Add files via upload

---
+++ data/keyring_settings_example.lua
@@ -0,0 +1,13 @@
+-- Keyring Addon State File
+-- Generated automatically - do not edit manually
+-- Character Server ID: 233358
+
+local state = {
+    timestamps = {[3137] = 0, [3300] = 1754580252, [3212] = 1754519631},
+    owned = {[3137] = true, [3300] = false, [3212] = true},
+    storage_canteens = 3,
+    last_canteen_time = 0,
+    dynamis_d_entry_time = 0
+}
+
+return state
